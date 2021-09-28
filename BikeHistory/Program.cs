8d8374a7b952ccaa0d759ff1060c711f1b39130c
vidarandrebo <va.bo@stud.uis.no>
2021-09-29 00:04:37 +0200

    small change

--- BikeHistory/Program.cs
+++ BikeHistory/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BikeHistory
 {
@@ -14,6 +8,9 @@ namespace BikeHistory
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+        }
+        public int test3() {
+            return int.Parse("1");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
