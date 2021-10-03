752209f139ccb7465a0b352cf2e008e48ed6c6a9
vidarandrebo <va.bo@stud.uis.no>
2021-10-03 21:46:03 +0200

    added helper functions

--- BikeHistory/webpack.config.js
+++ BikeHistory/webpack.config.js
@@ -1,28 +1,26 @@
 const path = require('path');
 const { VueLoaderPlugin } = require("vue-loader");
-var webpack = require('webpack');
-var featureFlags = new webpack.DefinePlugin({
-  __VUE_OPTIONS_API__: true,
-  __VUE_PROD_DEVTOOLS__: false
+const webpack = require('webpack');
+const featureFlags = new webpack.DefinePlugin({
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false
 });
 
 module.exports = {
     entry: './wwwroot/js/script.js',
-    //mode: "development",
-    mode: "production",
+    mode: "development",
+    //mode: "production",
     module: {
         rules: [
-        // ... other rules
-                {
-                    test: /\.vue$/,
-                    loader: 'vue-loader'
-                }
-            ]
-        },
-        plugins: [
-        // make sure to include the plugin!
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
         new VueLoaderPlugin(), featureFlags
-        ],
+    ],
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js'
@@ -31,6 +29,6 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'wwwroot/js/'),
-  },
+    },
 };
 
---
+++ BikeHistory/wwwroot/js/helperfunctions.js
@@ -0,0 +1,42 @@
+//the get* functions defined here are used in serveral different components and are therefore not just a mehthod within the component.
+async function getUsername() {
+    let response = await fetch('/username', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    let result = await response.json();
+    let username = result["username"];
+    return username;
+}
+
+async function postUser() {
+    let response = await fetch('/User', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+                "id": 4,
+                "userName": "vidar",
+                "password": "123"
+        }),
+
+    });
+    let result = await response;
+}
+
+
+
+function pathString(path) {
+    let pathList = ["global", "user", "login", "register", "settings"]
+    for (let i = 0; i < pathList.length; i++) {
+        if (path.includes(pathList[i])) {
+            return pathList[i];
+        }
+    }
+    return "following"
+
+}
+
