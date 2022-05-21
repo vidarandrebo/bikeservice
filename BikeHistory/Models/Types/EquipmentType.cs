8c2e694f10cab1af8dd31961403a050148220827
vidarandrebo <vidarandre.bo@protonmail.com>
2022-05-22 00:24:28 +0200

    init

--- BikeHistory/Models/Types/EquipmentType.cs
+++ BikeHistory/Models/Types/EquipmentType.cs
@@ -7,6 +7,10 @@ public class EquipmentType
     public Category Category { get; set; }
     public Guid UserId { get; set; }
 
+    public EquipmentTypeDto CreateDto()
+    {
+        return new EquipmentTypeDto(Id, Name, Category);
+    }
     public EquipmentType(string name, Category category, Guid userId)
     {
         Name = name;
