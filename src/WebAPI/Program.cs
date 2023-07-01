755fc7500049883e6b9c2a9b9ef607e6eb1c9665
vidarandrebo <vidarandre.bo@protonmail.com>
2023-07-01 21:10:52 +0200

    feat: migrations apply automatically at startup

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddApplicationServices();
 
 builder.Services.AddRouting();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication();
@@ -27,6 +26,8 @@ builder.Host.UseSerilog();
 
 var app = builder.Build();
 
+await app.Services.ApplyMigrations();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -34,20 +35,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-
 app.UseRouting();
-
-app.UseCors("mypolicy");
 
 app.UseAuthentication();
 
 app.UseDefaultFiles();
+
 app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.MapFallbackToFile("index.html");
+
 
 app.Run();
