de71a4222bc137260413e559d81e3436c82c2abf
Vidar André Bø <vidarandre.bo@protonmail.com>
2024-09-15 20:41:38 +0200

    style: footer min height

--- src/Client/src/Components/PageFooter.vue
+++ src/Client/src/Components/PageFooter.vue
@@ -7,7 +7,7 @@ export default defineComponent({
 </script>
 
 <template>
-    <footer class="sticky flex items-center justify-center w-full bg-blue-500 h-12">
+    <footer class="sticky flex items-center justify-center w-full bg-blue-500 min-h-12">
         <a href="https://github.com/vidarandrebo" class="text-white">https://github.com/vidarandrebo</a>
     </footer>
 </template>
