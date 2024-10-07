dee501723c9dd181a32906edc31faef0677ef4ff
Vidar André Bø <vidarandre.bo@protonmail.com>
2024-10-07 21:37:09 +0200

    style: settings-page style

--- src/Client/src/Components/Settings/SettingsMenu.vue
+++ src/Client/src/Components/Settings/SettingsMenu.vue
@@ -1,17 +1,23 @@
 <template>
     <div class="flex flex-col">
-        <RouterLink to="/settings/account">Account</RouterLink>
-        <RouterLink to="/settings/bike-types">Bike Types</RouterLink>
-        <RouterLink to="/settings/part-types">Part Types</RouterLink>
-        <RouterLink to="#">Something</RouterLink>
-        <RouterLink to="#">Else</RouterLink>
+        <SettingsMenuRouterLink to="/settings/account">
+            <p class="p-1">Account</p>
+        </SettingsMenuRouterLink>
+        <SettingsMenuRouterLink to="/settings/bike-types">
+            <p class="p-1">Bike Types</p>
+        </SettingsMenuRouterLink>
+        <SettingsMenuRouterLink to="/settings/part-types">
+            <p class="p-1">Part Types</p>
+        </SettingsMenuRouterLink>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SettingsMenuRouterLink from "../Common/SettingsMenuRouterLink.vue";
 
 export default defineComponent({
-    name: "SettingsMenu"
+    name: "SettingsMenu",
+    components: { SettingsMenuRouterLink }
 });
 </script>
--- src/Client/src/Components/Settings/SettingsPage.vue
+++ src/Client/src/Components/Settings/SettingsPage.vue
@@ -1,11 +1,15 @@
 <template>
     <main>
-        <article class="flex flex-row justify-between min-w-full">
-            <SettingsMenu></SettingsMenu>
+        <article class="flex flex-row justify-between max-w-screen-lg w-full p-4">
+            <div>
+                <HeadingH1 class="">Settings</HeadingH1>
+                <SettingsMenu></SettingsMenu>
+            </div>
             <RouterView class="flex flex-row grow"></RouterView>
         </article>
     </main>
 </template>
 <script setup lang="ts">
 import SettingsMenu from "./SettingsMenu.vue";
+import HeadingH1 from "../Common/Headings/HeadingH1.vue";
 </script>
