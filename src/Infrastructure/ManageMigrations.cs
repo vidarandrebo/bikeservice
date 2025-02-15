35ae85da645393f5496094d4d9b7b18037a1bbac
Vidar André Bø <vidarandre.bo@protonmail.com>
2025-02-15 20:47:59 +0100

    feat: only run migrations in development

--- src/Infrastructure/ManageMigrations.cs
+++ src/Infrastructure/ManageMigrations.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Npgsql;
 using Serilog;
 
 namespace BikeService.Infrastructure;
@@ -15,33 +14,7 @@ public static class ManageMigrations
     {
         using (var scope = serviceProvider.CreateScope())
         {
-            if (environment.IsProduction())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                Log.Logger.Information("Running migration on database");
-                var tries = 0;
-                while (true)
-                {
-                    tries++;
-                    try
-                    {
-                        db.Database.Migrate();
-                        break;
-                    }
-                    catch (NpgsqlException)
-                    {
-                        Log.Logger.Warning("Database not accessible, trying again in 5 seconds");
-                        await Task.Delay(5000);
-                    }
-
-                    if (tries > 5)
-                    {
-                        Log.Logger.Error("Migrations did not apply, continuing");
-                        break;
-                    }
-                }
-            }
-            else
+            if (environment.IsDevelopment())
             {
                 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                 Log.Logger.Information("Running migration on database");
