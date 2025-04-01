5e8005ac8d53f591e5166c8cc195793737294da2
Vidar André Bø <56341952+vidarandrebo@users.noreply.github.com>
2025-04-01 20:46:49 +0200

    cd: initial deploy pipeline (#128)
    * initial pipeline
    
    * upload artifact to test
    
    * enable publish on prchange
    
    * change to trigger change
    
    * only run on merge

--- src/Client/src/Components/MainPage.vue
+++ src/Client/src/Components/MainPage.vue
@@ -1,11 +1,10 @@
 <template>
     <main>
+        <HeadingH1>BikeService</HeadingH1>
         <div v-if="!user.email">
-            <h1>BikeService</h1>
             <p>Welcome to BikeService</p>
         </div>
         <div v-if="user.email">
-            <h1>BikeService</h1>
             <p>Welcome {{ user.email }}</p>
         </div>
     </main>
@@ -13,6 +12,7 @@
 <script setup lang="ts">
 import { DefaultUserDependency } from "../Models/Auth/User.ts";
 import { inject } from "vue";
+import HeadingH1 from "./Common/Headings/HeadingH1.vue";
 
 const { user } = inject("user", DefaultUserDependency, true);
 </script>
