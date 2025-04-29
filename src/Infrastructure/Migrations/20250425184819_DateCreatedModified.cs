79e7c04c931f541ae77b7be2727f579f461629ca
Vidar André Bø <vidarandre.bo@protonmail.com>
2025-04-29 18:31:49 +0200

    chore: migrations

---
+++ src/Infrastructure/Migrations/20250425184819_DateCreatedModified.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateCreatedModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "ServiceNotes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "ServiceNotes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Parts",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Parts",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "EquipmentTypes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "EquipmentTypes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Bikes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Bikes",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "ServiceNotes");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "ServiceNotes");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Parts");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Parts");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "EquipmentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "EquipmentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Bikes");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Bikes");
+        }
+    }
+}
