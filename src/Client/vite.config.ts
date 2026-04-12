6295deb0081f8f68828c79c600fc71e0291a7454
Vidar André Bø <56341952+vidarandrebo@users.noreply.github.com>
2026-04-12 20:03:48 +0200

    Build: Use pnpm (#150)
    * build: init pnpm
    
    * build: explicitly install bulma
    
    * actions: use pnpm in workflow
    
    * chore: indent
    
    * actions: use pnpm 10

--- src/Client/vite.config.ts
+++ src/Client/vite.config.ts
@@ -4,6 +4,14 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    css: {
+    preprocessorOptions: {
+      scss: {
+        // Dart Sass options
+          api: 'modern-compiler'
+        }
+      }
+    },
     server: {
         proxy: {
             "/api": {
